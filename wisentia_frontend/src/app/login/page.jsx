e38d7cc0a8e27eb0ee81068a62d05f870b050b1f
n0tnow <110254761+n0tnow@users.noreply.github.com>
2025-04-19 19:16:07 +0300

    initial commit

--- wisentia_frontend/src/app/login/page.jsx
+++ wisentia_frontend/src/app/login/page.jsx
@@ -1,4 +1,7 @@
-import LoginPage from './LoginClient';
+import dynamic from 'next/dynamic';
+
+// SSR hatası almamak için dinamik olarak yüklenir
+const LoginPage = dynamic(() => import('./LoginClient'), { ssr: false });
 
 export default function Page() {
   return <LoginPage />;
